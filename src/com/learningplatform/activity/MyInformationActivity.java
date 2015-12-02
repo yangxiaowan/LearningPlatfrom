f6468c2330d9f40662d9587d6f930e9d8651feeb
yangxiaowan <632024528@qq.com>
2015-12-02 19:53:28 +0800

    2015/12/2/2

---
+++ src/com/learningplatform/activity/MyInformationActivity.java
@@ -0,0 +1,13 @@
+package com.learningplatform.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import com.example.demo1.R;
+public class MyInformationActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.myinformation_layout);
+	}
+}
--- src/com/learningplatfrom/fragment/MySettingFragment.java
+++ src/com/learningplatfrom/fragment/MySettingFragment.java
@@ -1,15 +1,18 @@
 package com.learningplatfrom.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.demo1.R;
+import com.learningplatform.activity.MyInformationActivity;
 
 public class MySettingFragment extends Fragment {
-
+	private ImageView mHeadImageView = null;
 	public MySettingFragment() {
 	}
 	@Override
@@ -17,4 +20,18 @@ public class MySettingFragment extends Fragment {
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.mysetting_fragment_layout, container,false);
 	}
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		mHeadImageView = (ImageView)getView().findViewById(R.id.mysetting_headimage_imageview);
+		mHeadImageView.setOnClickListener(mHeadImageViewClickListener);
+	}
+	private View.OnClickListener mHeadImageViewClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			Intent intent = new Intent();
+			intent.setClass(getActivity().getApplicationContext(), MyInformationActivity.class);
+			startActivity(intent);
+		}
+	};
 }
